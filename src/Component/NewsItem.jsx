ca41f45dd71810d3b019e55bee97f3d42d4eb0e3
Saksham2k3s <sakshamshrivastav58@gmail.com>
2023-09-10 14:00:40 +0530

    Change Read more

--- src/Component/NewsItem.jsx
+++ src/Component/NewsItem.jsx
@@ -44,7 +44,7 @@ function NewsItem(props) {
                 target="_blank"
                 className="btn"
               >
-                Read More
+                Read M
               </Link>
 
               <Checkbox
